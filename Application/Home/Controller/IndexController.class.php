f8f480103f7438fd2cb4ad363c46421474db49f9
xiaomo <1549169735@qq.com>
2016-10-30 23:37:09 +0800

    新增项目

---
+++ Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+    	//加载模板
+      	$this->display();
+    }
+
+  
+}
