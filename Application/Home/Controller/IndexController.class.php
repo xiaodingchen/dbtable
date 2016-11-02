da53e6695215d211cb530b8d6d7b006687627df9
xiaomo <1549169735@qq.com>
2016-11-02 22:57:09 +0800

    test

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,10 +1,43 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+use Think\Db\Driver\Mysqli;
 class IndexController extends Controller {
     public function index(){
-    	//加载模板
-      	$this->display();
+        $config['hostname'] = '127.0.0.1';
+        $config['username'] = 'root';
+        $config['password'] = 'root';
+        $config['database'] = 'blog';
+        $config['hostport'] = 3306;
+    	$obj = new Mysqli($config);
+    	$tables = $obj->getTables();
+    	$fields = $obj->getFields('bg_article');
+    	$indexs = $obj->query('SHOW index FROM bg_article');
+    	$tmp = [];
+    	$tmp['columns'] = $fields;
+    	foreach ($indexs as $index)
+    	{
+    	    if($index['Non_unique'] == 0 && $index['Key_name'] != 'PRIMARY')
+    	    {
+    	        $tmp['unique'][$index['Key_name']][] = $index['Column_name'];
+    	    }
+    	    
+    	    if($index['Non_unique'] == 1)
+    	    {
+    	        $tmp['index'][$index['Key_name']][] = $index['Column_name'];
+    	    }
+    	    
+    	    if($index['Non_unique'] == 0 && $index['Key_name'] == 'PRIMARY')
+    	    {
+    	        $tmp['primary'] = $index['Column_name'];
+    	    }
+    	} 
+    	
+    	echo '<pre>';
+    	print_r($tmp);
+    	var_dump($fields);
+    	exit;
+//     	var_export($expression)
     }
 
   
